8e293c5a8110ab88bc5d36d38aec220cdf68fb45
DavAug <david.augustin@gmx.net>
2020-10-27 09:08:16 +0000

    flake 8

--- seirmo/__init__.py
+++
@@ -1,6 +0,0 @@
-"""seirmo is a Epidemiology SEIR modelling library.
-
-"""
-
-# Import version info
-from .version_info import VERSION_INT, VERSION  # noqa
--- seirmo/tests/test_dummy.py
+++ seirmo/tests/test_dummy.py
@@ -2,9 +2,10 @@ import unittest
 
 from seirmo.dummy import dummy_function
 
+
 class TestDummyFunction(unittest.TestCase):
     '''
     Test Dummy function
     '''
     def test_output(self):
-        self.assertEqual(dummy_function(),42)
+        self.assertEqual(dummy_function(), 42)
