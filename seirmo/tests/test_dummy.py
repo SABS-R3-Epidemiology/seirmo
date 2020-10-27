1f85e99f2d392ea574269ee39da50ff5ac018576
FarmHJ <huijia-farm_95@hotmail.com>
2020-10-27 03:24:26 +0000

    add comments

--- seirmo/tests/test_dummy.py
+++ seirmo/tests/test_dummy.py
@@ -3,5 +3,8 @@ import unittest
 from seirmo.dummy import dummy_function
 
 class TestDummyFunction(unittest.TestCase):
+    '''
+    Test Dummy function
+    '''
     def test_output(self):
         self.assertEqual(dummy_function(),42)
