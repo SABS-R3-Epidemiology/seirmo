001c05855bd02f6bf0093b8d4f09dfd33bea330d
DavAug <david.augustin@gmx.net>
2020-10-27 09:08:22 +0000

    remove tests init

--- seirmo/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-#
-# Test module for seirmo.
-#
-# To run all tests, use ``python -m unittest discover``.
-#
-# To run a particular test, use e.g.
-#  ``python -m unittest seirmo.tests.test_dummy``.
