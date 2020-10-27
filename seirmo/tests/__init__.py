25a17cf63aa5f89cfd54dfb0ea0213133f3256c2
FarmHJ <huijia-farm_95@hotmail.com>
2020-10-27 03:59:36 +0000

    Add __init__ file for unit testing

---
+++ seirmo/tests/__init__.py
@@ -0,0 +1,7 @@
+#
+# Test module for seirmo.
+#
+# To run all tests, use ``python -m unittest discover``.
+#
+# To run a particular test, use e.g.
+#  ``python -m unittest seirmo.tests.test_dummy``.
