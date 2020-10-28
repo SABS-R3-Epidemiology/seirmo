afdd287dd334f163b2c6af883952a4deda1cad3c
FarmHJ <huijia-farm_95@hotmail.com>
2020-10-28 14:33:35 +0000

    add simulation.py

---
+++ seirmo/simulation.py
@@ -0,0 +1,16 @@
+#
+# This file is part of seirmo (https://github.com/SABS-R3-Epidemiology/seirmo/)
+# which is released under the BSD 3-clause license. See accompanying LICENSE.md
+# for copyright notice and full license details.
+#
+
+class SimulationController(ForwardModel):
+    """SimulationController Class:
+    """
+
+    def __init__(self,simulation_time: tuple):
+        super(ForwardModel, self).__init__()
+
+    def run(self, parameters, times):
+
+        return output
