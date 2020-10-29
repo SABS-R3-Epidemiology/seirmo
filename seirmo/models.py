c428a2ff9b63cec26b5d1469f95634b2d6ba8d33
Siting Miao <siting.miao@linacre.ox.ac.uk>
2020-10-29 09:45:04 +0000

    #24 Update SEIRModel to calculate incidence

--- seirmo/models.py
+++ seirmo/models.py
@@ -5,14 +5,10 @@
 #
 
 from scipy.integrate import solve_ivp
-from scipy.interpolate import interp1d
-
-import scipy.integrate as integrate
 
 
 class ForwardModel(object):
     """ForwardModel Class:
-
     Abstract base class for any models.
     """
 
@@ -23,11 +19,9 @@ class ForwardModel(object):
         """
         Forward simulation of a model for a given time period
         with given parameters
-
         Returns a sequence of length ``n_times`` (for single output problems)
         or a NumPy array of shape ``(n_times, n_outputs)`` (for multi-output
         problems), representing the values of the model at the given ``times``.
-
         Parameters
         ----------
         parameters: sequence of numerics
@@ -53,49 +47,42 @@ class SEIRModel(ForwardModel):
         \frac{dR(t)}{dt} = \gamma I(t),
     where :math:`S(0) = S_0, E(0) = E_0, I(O) = I_0, R(0) = R_0`
     are also parameters of the model.
-    The instantaneous incidence at time :math:`t` is given by:
-    .. math::
-        \frac{dC(t)}{dt} = \kappa E(t),
-    and the weekly incidence will be given by its integral.
     """
 
     def __init__(self):
         super(SEIRModel, self).__init__()
 
     def _right_hand_side(self, t, y, c):
-        # Assuming y = [S, E, I, R, C] (the dependent variables in the model)
+        # Assuming y = [S, E, I, R] (the dependent variables in the model)
         # Assuming the parameters are ordered like
-        # parameters = [S0, E0, I0, R0, C0, beta, kappa, gamma]
+        # parameters = [S0, E0, I0, R0, beta, kappa, gamma]
         # Let c = [beta, kappa, gamma]
         #  = [parameters[0], parameters[1], parameters[2]],
         # then beta = c[0], kappa = c[1], gamma = c[2]
 
         # Construct the derivative functions of the system of ODEs
 
-        s, e, i, _, _ = y
+        s, e, i, _ = y
         beta, kappa, gamma = c
         dydt = [-beta * s * i, beta * s * i - kappa * e,
-                kappa * e - gamma * i, gamma * i, kappa * e]
+                kappa * e - gamma * i, gamma * i]
 
         return dydt
 
-    # Return the number of new cases between provided times
-    def _compute_incidences(self, n_incidence, times, t_i, t_f):
-        n_incidence_interp = interp1d(times, n_incidence, kind='cubic')
-        return integrate.quad(n_incidence_interp, t_i, t_f)
-
-    def simulate(self, parameters, times, t_i, t_f, return_incidence=False):
+    def simulate(self, parameters, times, return_incidence=False):
 
         # Define time spans, initial conditions, and constants
-        y_init = parameters[0:5]
-        c = parameters[5:]
+        y_init = parameters[0:4]
+        c = parameters[4:]
 
         # Solve the system of ODEs
         sol = solve_ivp(lambda t, y: self._right_hand_side(t, y, c),
                         [times[0], times[-1]], y_init, t_eval=times)
 
         if return_incidence is False:
-            return sol['y'][0:4, :].transpose()
+            return sol['y'].transpose()
         elif return_incidence is True:
-            n_incidence = sol['y'][4:, :]
-            return self._compute_incidences(n_incidence, times, t_i, t_f)
+            # Return incitance = (I(t_final) + R(t_final))
+            #       - (I(t_initial) + R(t_initial))
+            return (sol['y'][2, -1] + sol['y'][3, -1]) - (
+                sol['y'][2, 0] + sol['y'][3, 0])
