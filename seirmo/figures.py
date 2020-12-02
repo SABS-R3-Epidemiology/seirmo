c47cfda95ada9392bb768c2622cbe3f42dd61f80
Siting Miao <siting.miao@linacre.ox.ac.uk>
2020-12-02 14:43:02 +0000

    #36 Add tests

--- seirmo/figures.py
+++ seirmo/figures.py
@@ -6,49 +6,56 @@
 
 import plotly.graph_objects as go
 
-class IncidenceNumberPlot(object):
 
+class IncidenceNumberPlot(object):
+    """
+
+    Creates incidence number plots for the app
+
+    """
     def __init__(self):
         super(IncidenceNumberPlot, self).__init__()
 
         # Instantiate an empty plotly figure
-        fig = go.Figure()
-    
-    def add_data(self, pandas_DataFrame, time_key='Time', inc_key='Incidence Number'):
-        
+        self._fig = go.Figure()
+
+    def add_data(self, pandas_DataFrame, time_key='Time',
+                 inc_key='Incidence Number'):
+
         # Plot a bar chart for the data
-        fig.add_trace(
+        self._fig.add_trace(
             go.Bar(
-                x = pandas_DataFrame[time_key],
-                y = pandas_DataFrame[inc_key],
-                name = 'cases'
+                x=pandas_DataFrame[time_key],
+                y=pandas_DataFrame[inc_key],
+                name='cases'
             )
         )
 
         # Add axis labels
-        fig.update_layout(
-            xaxis_title = time_key
-            yaxis_title = inc_key
+        self._fig.update_layout(
+            xaxis_title=time_key,
+            yaxis_title=inc_key
         )
 
-    def add_simulation(self, pandas_DataFrame, time_key='Time', inc_key='Incidence Number')
-        
+    def add_simulation(self, pandas_DataFrame, time_key='Time',
+                       inc_key='Incidence Number'):
+
         # Plot a bar chart for the simulation
-        fig.add_trace(
+        self._fig.add_trace(
             go.Bar(
-                x = pandas_DataFrame[time_key],
-                y = pandas_DataFrame[inc_key],
-                name = 'simulation'
+                x=pandas_DataFrame[time_key],
+                y=pandas_DataFrame[inc_key],
+                name='simulation'
             )
         )
 
         # Add axis labels
-        fig.update_layout(
-            xaxis_title = time_key
-            yaxis_title = inc_key
+        self._fig.update_layout(
+            xaxis_title=time_key,
+            yaxis_title=inc_key
         )
-    
-    def show(self)
+
+    def show(self):
 
         # Display the figure
-        fig.show()
+        self._fig.show()
--- seirmo/tests/test_figures.py
+++ seirmo/tests/test_figures.py
@@ -7,8 +7,10 @@
 import unittest
 
 import numpy as np
+import pandas as pd
 
 import seirmo as se
+
 
 class IncidenceNumberPlot(unittest.TestCase):
     """
@@ -18,7 +20,39 @@ class IncidenceNumberPlot(unittest.TestCase):
         se.IncidenceNumberPlot()
 
     def test_add_data(self):
-    
-    def test_add_simulation(self):
+        pandas_DataFrame = pd.DataFrame({'Time': [0, 1, 2, 3, 4, 5, 6],
+                                         'Incidence Number': [1, 2, 3, 4, 5, 6, 7]}) # noqa
+        time_key = 'Time'
+        inc_key = 'Incidence Number'
+        test_plot = se.IncidenceNumberPlot()
+        test_plot.add_data(pandas_DataFrame, time_key=time_key,
+                           inc_key=inc_key)
 
-    def test_show(self):
+        # Test the data in the figure is the same as what we give
+        np.testing.assert_array_equal(
+            np.array(
+                [test_plot._fig['data'][0]['x'],
+                 test_plot._fig['data'][0]['y']]),
+            np.array(
+                [np.array([0, 1, 2, 3, 4, 5, 6]),
+                 np.array([1, 2, 3, 4, 5, 6, 7])])
+        )
+
+    def test_add_simulation(self):
+        pandas_DataFrame = pd.DataFrame({'Time': [0, 1, 2, 3, 4, 5, 6],
+                                         'Incidence Number': [1, 2, 3, 4, 5, 6, 7]}) # noqa
+        time_key = 'Time'
+        inc_key = 'Incidence Number'
+        test_plot = se.IncidenceNumberPlot()
+        test_plot.add_simulation(pandas_DataFrame, time_key=time_key,
+                                 inc_key=inc_key)
+
+        # Test the data in the figure is the same as what we give
+        np.testing.assert_array_equal(
+            np.array(
+                [test_plot._fig['data'][0]['x'],
+                 test_plot._fig['data'][0]['y']]),
+            np.array(
+                [np.array([0, 1, 2, 3, 4, 5, 6]),
+                 np.array([1, 2, 3, 4, 5, 6, 7])])
+        )
