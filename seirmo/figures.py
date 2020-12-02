d62d92f7ac45529383d672fd8ec68a206d299546
Siting Miao <siting.miao@linacre.ox.ac.uk>
2020-12-02 19:17:53 +0000

    #36 Modifications

--- seirmo/figures.py
+++ seirmo/figures.py
@@ -19,41 +19,79 @@ class IncidenceNumberPlot(object):
         # Instantiate an empty plotly figure
         self._fig = go.Figure()
 
-    def add_data(self, pandas_DataFrame, time_key='Time',
+        """
+        Add a bar plot trace for the given incidence number data
+        in the figure
+
+        Parameters
+        ----------
+        data: pandas.DataFrame
+        time_key: docstring
+        inc_key: docstring
+        """
+    def add_data(self, data, time_key='Time',
                  inc_key='Incidence Number'):
 
         # Plot a bar chart for the data
         self._fig.add_trace(
             go.Bar(
-                x=pandas_DataFrame[time_key],
-                y=pandas_DataFrame[inc_key],
+                x=data[time_key],
+                y=data[inc_key],
                 name='cases'
             )
         )
 
-        # Add axis labels
-        self._fig.update_layout(
-            xaxis_title=time_key,
-            yaxis_title=inc_key
-        )
+        # Add axis labels; Raise error when the x and y axis labels
+        # do not match the old ones if they already exist;
+        # otherwise update the labels
+        old_x_label = self._fig['layout']['xaxis']['anchor']
+        old_y_label = self._fig['layout']['yaxis']['anchor']
+        if not (old_x_label or old_y_label):
+            if old_x_label != time_key or old_y_label != inc_key:
+                raise ValueError(
+                    'The x and y labels do not match the old ones.')
+        else:
+            self._fig.update_layout(
+                xaxis_title=time_key,
+                yaxis_title=inc_key
+            )
 
-    def add_simulation(self, pandas_DataFrame, time_key='Time',
+        """
+        Add a bar plot trace for the simulated incidence numbers
+        in the figure
+
+        Parameters
+        ----------
+        data: pandas.DataFrame
+        time_key: docstring
+        inc_key: docstring
+        """
+    def add_simulation(self, data, time_key='Time',
                        inc_key='Incidence Number'):
 
         # Plot a bar chart for the simulation
         self._fig.add_trace(
             go.Bar(
-                x=pandas_DataFrame[time_key],
-                y=pandas_DataFrame[inc_key],
+                x=data[time_key],
+                y=data[inc_key],
                 name='simulation'
             )
         )
 
-        # Add axis labels
-        self._fig.update_layout(
-            xaxis_title=time_key,
-            yaxis_title=inc_key
-        )
+        # Add axis labels; Raise error when the x and y axis labels
+        # do not match the old ones if they already exist;
+        # otherwise update the labels
+        old_x_label = self._fig['layout']['xaxis']['anchor']
+        old_y_label = self._fig['layout']['yaxis']['anchor']
+        if not (old_x_label or old_y_label):
+            if old_x_label != time_key or old_y_label != inc_key:
+                raise ValueError(
+                    'The x and y labels do not match the old ones.')
+        else:
+            self._fig.update_layout(
+                xaxis_title=time_key,
+                yaxis_title=inc_key
+            )
 
     def show(self):
 
--- seirmo/tests/test_figures.py
+++ seirmo/tests/test_figures.py
@@ -5,6 +5,7 @@
 #
 
 import unittest
+from unittest.mock import patch
 
 import numpy as np
 import pandas as pd
@@ -12,7 +13,7 @@ import pandas as pd
 import seirmo as se
 
 
-class IncidenceNumberPlot(unittest.TestCase):
+class TestIncidenceNumberPlot(unittest.TestCase):
     """
     Test the 'IncidenceNumberPlot' class.
     """
@@ -20,39 +21,61 @@ class IncidenceNumberPlot(unittest.TestCase):
         se.IncidenceNumberPlot()
 
     def test_add_data(self):
-        pandas_DataFrame = pd.DataFrame({'Time': [0, 1, 2, 3, 4, 5, 6],
+        data = pd.DataFrame({'Time': [0, 1, 2, 3, 4, 5, 6],
                                          'Incidence Number': [1, 2, 3, 4, 5, 6, 7]}) # noqa
         time_key = 'Time'
         inc_key = 'Incidence Number'
         test_plot = se.IncidenceNumberPlot()
-        test_plot.add_data(pandas_DataFrame, time_key=time_key,
+        test_plot.add_data(data, time_key=time_key,
                            inc_key=inc_key)
 
-        # Test the data in the figure is the same as what we give
-        np.testing.assert_array_equal(
-            np.array(
-                [test_plot._fig['data'][0]['x'],
-                 test_plot._fig['data'][0]['y']]),
-            np.array(
-                [np.array([0, 1, 2, 3, 4, 5, 6]),
-                 np.array([1, 2, 3, 4, 5, 6, 7])])
-        )
+        # Test the times in the figure is the same as what we give
+        np.testing.assert_array_equal(test_plot._fig['data'][0]['x'],
+                                      np.array([0, 1, 2, 3, 4, 5, 6]))
+
+        # Test the incidences in the figure is the same as what we give
+        np.testing.assert_array_equal(test_plot._fig['data'][0]['y'],
+                                      np.array([1, 2, 3, 4, 5, 6, 7]))
+
+        # Test that error will be raised when axis labels not match
+        data2 = pd.DataFrame({'time': [0, 1, 2, 3, 4, 5, 6],
+                                         'incidence number': [1, 2, 3, 4, 5, 6, 7]}) # noqa
+        time_key = 'time'
+        inc_key = 'incidence number'
+
+        with self.assertRaises(ValueError):
+            test_plot.add_data(data2, time_key=time_key,
+                               inc_key=inc_key)
 
     def test_add_simulation(self):
-        pandas_DataFrame = pd.DataFrame({'Time': [0, 1, 2, 3, 4, 5, 6],
+        data = pd.DataFrame({'Time': [0, 1, 2, 3, 4, 5, 6],
                                          'Incidence Number': [1, 2, 3, 4, 5, 6, 7]}) # noqa
         time_key = 'Time'
         inc_key = 'Incidence Number'
         test_plot = se.IncidenceNumberPlot()
-        test_plot.add_simulation(pandas_DataFrame, time_key=time_key,
+        test_plot.add_simulation(data, time_key=time_key,
                                  inc_key=inc_key)
 
-        # Test the data in the figure is the same as what we give
-        np.testing.assert_array_equal(
-            np.array(
-                [test_plot._fig['data'][0]['x'],
-                 test_plot._fig['data'][0]['y']]),
-            np.array(
-                [np.array([0, 1, 2, 3, 4, 5, 6]),
-                 np.array([1, 2, 3, 4, 5, 6, 7])])
-        )
+        # Test the times in the figure is the same as what we give
+        np.testing.assert_array_equal(test_plot._fig['data'][0]['x'],
+                                      np.array([0, 1, 2, 3, 4, 5, 6]))
+
+        # Test the incidences in the figure is the same as what we give
+        np.testing.assert_array_equal(test_plot._fig['data'][0]['y'],
+                                      np.array([1, 2, 3, 4, 5, 6, 7]))
+
+        # Test that error will be raised when axis labels not match
+        data2 = pd.DataFrame({'time': [0, 1, 2, 3, 4, 5, 6],
+                                         'incidence number': [1, 2, 3, 4, 5, 6, 7]}) # noqa
+        time_key = 'time'
+        inc_key = 'incidence number'
+
+        with self.assertRaises(ValueError):
+            test_plot.add_simulation(data2, time_key=time_key,
+                                     inc_key=inc_key)
+
+    def test_show(self):
+        test_plot = se.IncidenceNumberPlot()
+        with patch('plotly.graph_objects.Figure.show') as show_patch:
+            test_plot.show()
+            assert show_patch.called
