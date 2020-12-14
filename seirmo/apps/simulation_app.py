84e8e946f26b5ab9c1424cde891c4639d9c2483a
FarmHJ <huijia-farm_95@hotmail.com>
2020-12-14 10:43:41 +0000

    #38 create SimulationApp class

---
+++ seirmo/apps/simulation_app.py
@@ -0,0 +1,36 @@
+#
+# This file is part of seirmo (https://github.com/SABS-R3-Epidemiology/seirmo/)
+# which is released under the BSD 3-clause license. See accompanying LICENSE.md
+# for copyright notice and full license details.
+#
+
+import seirmo as se
+
+class SimulationApp(object):
+    """SimulationApp Class:
+
+    Creates the SEIR model simulation app.
+
+    """
+
+    def __init__(self):
+        super(SimulationApp, self).__init__()
+
+        IncidenceNumberPlot = se.IncidenceNumberPlot()
+        SliderComponent = se._SliderComponent()
+
+    def add_data(self):
+        """
+        """
+        
+    def add_model(self, model, parameters_name):
+
+        for model_parameter in parameters_name:
+            SliderComponent.add_slider(slider_id = model_parameter,
+                                        min_value = 0,
+                                        max_value = 1)
+        
+        simulate = se.SimulationController(model,0,40)
+        initial_parameters_value = [0]*len(parameters_name)
+        data = simulate.run(initial_parameters_value, return_incidence=True)
+        IncidenceNumberPlot.add_data(data)
