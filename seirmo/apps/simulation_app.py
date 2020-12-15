2cd9958ed730909788c45b7a5c33befa9b0dcf98
FarmHJ <huijia-farm_95@hotmail.com>
2020-12-15 06:30:13 +0000

    #38 modify app class as requested

--- seirmo/apps/simulation_app.py
+++ seirmo/apps/simulation_app.py
@@ -4,12 +4,12 @@
 # for copyright notice and full license details.
 #
 
-import pandas as pd
-import seirmo as se
-
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+import pandas as pd
+
+import seirmo as se
 
 
 class SimulationApp(object):
@@ -22,55 +22,105 @@ class SimulationApp(object):
     def __init__(self):
         super(SimulationApp, self).__init__()
 
-        self.IncidenceNumberPlot = se.IncidenceNumberPlot()
-        self.SliderComponent = se._SliderComponent()
-
-        self.app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-        self.app.layout = dbc.Container(
-            dbc.Row(
-                [
-                    dbc.Col(
-                        dcc.Graph(
-                            figure=self.IncidenceNumberPlot._fig, id='fig')),
-                    dbc.Col(
-                        [dbc.Row(
-                            self.SliderComponent.group_sliders(
-                                self.slider_ids()[:4], 'init_value')),
-                         dbc.Row(
-                            self.SliderComponent.group_sliders(
-                                self.slider_ids()[4:], 'constant'))]
-                    )
-                ]
-            )
-        )
+        self._fig_plot = se.IncidenceNumberPlot()
+        self._slider_component = se._SliderComponent()
 
         self.simulation_start = 0
         self.simulation_end = 50
 
-    def add_data(self, data):
+    def _set_layout(self):
         """
+        Create the layout of the app.
         """
-        self.IncidenceNumberPlot.add_data(
-            data, time_key='Time', inc_key='Incidence Number')
+
+        self.app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+        self.app.layout = dbc.Container(
+            dbc.Row([
+                    dbc.Col([dcc.Graph(
+                            figure=self._fig_plot._fig, id='fig')]),
+                    dbc.Col([
+                        self._slider_component()])
+                ])
+            )
+
+    def add_data(self, data, time_key='Time', inc_key='Incidence Number'):
+        """
+        Plot figure for given incidence number data.
+
+        Parameters
+        ----------
+        data
+            A pandas.DataFrame with 2 columns, one being time points,
+            the other being incidence number.
+        time_key
+            Key label of the DataFrame which specifies the time points.
+            Defaults to 'Time'.
+        inc_key
+            Key label of the DataFrame which specifies the
+            incidence number. 
+            Defaults to 'Incidence Number'.
+        """
+
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError(
+                'Data has to be an instance of pandas.DataFrame.')
+
+        self._fig_plot.add_data(
+            data, time_key, inc_key)
 
     def add_model(self, model, parameters_name):
+        """
+        Plot figure of simulation for given model.
 
+        Parameters
+        ----------
+        model
+            A subclass of seirmo.ForwardModel class, which specifies
+            the model of simulation.
+        parameters_name
+            Name of parameters of model in list of strings.
+        """
+
+        if not issubclass(model, se.ForwardModel):
+            raise TypeError(
+                'Model has to be a subclass of seirmo.ForwardModel.')
+
+        parameters_name = list(parameters_name)
+
+        init_parameters = []
         for model_parameter in parameters_name:
-            self.SliderComponent.add_slider(slider_id=model_parameter,
-                                            min_value=0,
-                                            max_value=1)
+            model_parameter = str(model_parameter)
+            self._slider_component.add_slider(
+                slider_id=model_parameter,
+                min_value=0,
+                max_value=1)
+            init_parameters.append(
+                self._slider_component._sliders[model_parameter].value)
+
+        self._slider_component.group_sliders(parameters_name,'slider_group')
 
         self.simulate = se.SimulationController(
             model, self.simulation_start, self.simulation_end)
-        init_parameters = [0] * len(parameters_name)
+
         data = self.simulate.run(init_parameters, return_incidence=True)
         data = pd.DataFrame(data, columns=['Time', 'Incidence Number'])
-        self.IncidenceNumberPlot.add_simulation(data)
+        self._fig_plot.add_simulation(data)
 
     def slider_ids(self):
-        return self.SliderComponent.get_slider_ids()
+        """
+        Return the ids of sliders added to the app.
+        """
+        return self._slider.get_slider_ids()
 
     def update_simulation(self, parameters):
+        """
+        Update the figure with simulated data of new parameters.
+
+        Parameters
+        ----------
+        parameters
+            List of parameter values for simulation.
+        """
         data = self.simulate.run(parameters, return_incidence=True)
-        self.IncidenceNumberPlot._fig['data'][0]['y'] = data[:, 1]
+        self._fig_plot._fig['data'][0]['y'] = data[:, 1]
