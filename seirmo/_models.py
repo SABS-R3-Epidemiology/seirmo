5c00c9ca476c81feb047235cfd25dd886fd20e22
缪思婷 <miaositing@miusitingdeMacBook-Pro.local>
2021-01-27 19:29:43 +0800

    #66 Remove ForwardModel base class

--- seirmo/_models.py
+++ seirmo/_models.py
@@ -8,30 +8,6 @@ import numpy as np
 from scipy.integrate import solve_ivp
 
 
-class ForwardModel(object):
-    """ForwardModel Class:
-    Abstract base class for any models.
-    """
-
-    def __init__(self):
-        super(ForwardModel, self).__init__()
-
-    def simulate(self, parameters, times):
-        """
-        Forward simulation of a model for a given time period
-        with given parameters
-        Returns a sequence of length ``n_times`` (for single output problems)
-        or a NumPy array of shape ``(n_times, n_outputs)`` (for multi-output
-        problems), representing the values of the model at the given ``times``.
-
-        Parameters
-        ----------
-        parameters: sequence of numerics
-        times: sequence of numerics
-        """
-        raise NotImplementedError
-
-
 class SEIRModel(ForwardModel):
     r"""
     ODE model: deterministic SEIR
