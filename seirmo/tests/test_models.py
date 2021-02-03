b6bd84776574d43ad964c90c0db24c5ec4b48321
siting-miao <siting.miso@linacre.ac.uk>
2021-02-03 20:36:56 +0800

    #66 add test_simulate for SEIRModel

--- seirmo/tests/test_models.py
+++ seirmo/tests/test_models.py
@@ -100,7 +100,6 @@ class TestSEIRModel(unittest.TestCase):
 
     def test_simulate(self):
         model = se.SEIRModel()
-        n_outputs = 4
 
         initial_values = [0.9, 0, 0.1, 0]
         constants = [1, 1, 1]
@@ -109,39 +108,21 @@ class TestSEIRModel(unittest.TestCase):
         n_times = 10
         test_times = np.linspace(0, 10, num=n_times)
 
-        # Test the case when return_incidence=False
+        model.set_outputs(['S', 'I', 'Incidence'])
         output = model.simulate(test_parameters, test_times)
 
         # Check output shape
-        self.assertEqual(output.shape, (n_times, n_outputs))
+        self.assertEqual(output.shape, (n_times, 3))
 
         # Check that sum of states is one at all times
+        model.set_outputs(['S', 'E', 'I', 'R'])
+        output = model.simulate(test_parameters, test_times)
         total = np.sum(output, axis=1)
         expected = np.ones(shape=n_times)
         np.testing.assert_almost_equal(total, expected)
 
-    def test_simulate_return_incidence_true(self):
-        model = se.SEIRModel()
-
-        initial_values = [0.9, 0, 0.1, 0]
-        constants = [1, 1, 1]
-        test_parameters = initial_values + constants
-
-        n_times = 10
-        test_times = np.linspace(0, 10, num=n_times)
-
-        # Test the case when return_incidence=True
-        output = model.simulate(
-            test_parameters, test_times, return_incidence=True)
-
         # Check output shape
-        n_outputs = 5
-        self.assertEqual(output.shape, (n_times, n_outputs))
-
-        # Check that sum of states is one at all times
-        total = np.sum(output[:, :-1], axis=1)
-        expected = np.ones(shape=n_times)
-        np.testing.assert_almost_equal(total, expected)
+        self.assertEqual(output.shape, (n_times, 4))
 
 
 if __name__ == '__main__':
