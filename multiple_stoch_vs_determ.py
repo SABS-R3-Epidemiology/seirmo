b6122891a78ddd810b41883dee7b153dc5498e72
Luke Heirene <luke.heirene@dtc.ox.ac.uk>
2021-10-28 17:32:42 +0100

    added plot for multiple stochastic simulations comparison

--- multiple_stoch_vs_determ.py
+++ multiple_stoch_vs_determ.py
@@ -15,6 +15,9 @@ seir = ['S', 'E', 'I', 'R']
 iteration_number = 101
 iterations = np.linspace(0, 100, iteration_number)
 parameter_values = np.array([1000, 0, 20, 0, .1, .1, .1])
+infected_stoch_mean = np.zeros((iteration_number, 1))
+infected_stoch_2_5th_quartile = np.zeros((iteration_number, 1))
+infected_stoch_97_5th_quartile = np.zeros((iteration_number, 1))
 timeEnd = 10
 n_times = 10 * timeEnd + 1
 times = np.linspace(0, timeEnd, num=n_times)
@@ -23,8 +26,7 @@ times = np.linspace(0, timeEnd, num=n_times)
 stoch_infections = np.zeros((n_times, iteration_number))
 infection = np.ndarray((n_times,1))
 
-figure = se.plots.ConfigurablePlotter()
-figure.begin()
+
 i =0
 for iter in iterations:
     
@@ -33,17 +35,25 @@ for iter in iterations:
     
     output = model_stoch.simulate(np.array(parameter_values), times)
     stoch_infections[:, i] = output[:, 2]
-    figure.add_data_to_plot(times, stoch_infections[:, [i]], xlabel='time', ylabels='number of infected')
     i+=1
 
  # set up the deterministic model
 
+
+infected_stoch_mean = np.mean(stoch_infections, axis=0)
+infected_stoch_2_5th_quartile = np.percentile(stoch_infections, 2.5, axis=0)
+infected_stoch_97_5th_quartile = np.percentile(stoch_infections, 97.5, axis=0)
 model_determ._parameters.configure_parameters(parameter_values)
 model_determ.set_outputs(seir)
 output = model_determ.simulate(np.array(parameter_values), times)
 infection[:,0] = output[:, 2]
-print(infection.shape)
-figure.add_data_to_plot(times, infection, xlabel='time', ylabels='number of infected')
 
+figure = se.plots.ConfigurablePlotter()
+figure.begin()
+figure.add_data_to_plot(times, infection, xlabel='time', ylabels='number of infected')
+print(infected_stoch_mean.shape)
+figure.add_data_to_plot(times, infected_stoch_mean, xlabel='time', ylabels='number of infected')
+figure.add_data_to_plot(times, infected_stoch_2_5th_quartile, xlabel='time', ylabels='number of infected')
+figure.add_data_to_plot(times, infected_stoch_97_5th_quartile, xlabel='time', ylabels='number of infected')
 
 figure.show()
