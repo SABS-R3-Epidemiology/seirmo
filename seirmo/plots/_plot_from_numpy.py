e48701c5e15bc8392703f26706bf0ded604d3178
Kit Gallagher <gallagherkit@outlook.com>
2021-10-28 09:28:58 +0100

    initialise data collector with nans, lint

--- seirmo/plots/_plot_from_numpy.py
+++ seirmo/plots/_plot_from_numpy.py
@@ -1,3 +1,8 @@
+#
+# This file is part of seirmo (https://github.com/SABS-R3-Epidemiology/seirmo/)
+# which is released under the BSD 3-clause license. See accompanying LICENSE.md
+# for copyright notice and full license details.
+#
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +14,7 @@ class StochasticPlotter():
 
     def plot(self, labels: list,
              filename: str = 'SEIR_stochastic_simulation.png'):
-        new_data = self.remove_zero_row()
+        new_data = self._data
         time = new_data[:, 0]
         data = new_data[:, 1:]
         fig, axes = plt.subplots()
@@ -23,13 +28,10 @@ class StochasticPlotter():
         fig.savefig(filename)
         plt.show()
 
-    def remove_zero_row(self):
+    """def remove_zero_row(self):
         for i in range(self._data.shape[0]):
             if np.all(self._data[i, 1:] == 0):
                 break
         self._reduced_data = self._data[:i, :]
         return self._reduced_data
-
-# I still think it would be better to initialise the output array
-# with nans to avoid this though - as much as anything, having a row
-# of all zeros might be a valid solution! - KG
+"""
