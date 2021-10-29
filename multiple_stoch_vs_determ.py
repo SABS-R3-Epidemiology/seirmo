8387aa7f2aec4628b5e924ab252a33df45898af1
Luke Heirene <luke.heirene@dtc.ox.ac.uk>
2021-10-29 10:15:18 +0100

    stochastic simulation for Richard

--- multiple_stoch_vs_determ.py
+++ multiple_stoch_vs_determ.py
@@ -18,7 +18,7 @@ parameter_values = np.array([1000, 0, 20, 0, .1, .1, .1])
 infected_stoch_mean = np.zeros((iteration_number, 1))
 infected_stoch_2_5th_quartile = np.zeros((iteration_number, 1))
 infected_stoch_97_5th_quartile = np.zeros((iteration_number, 1))
-timeEnd = 10
+timeEnd = 20
 n_times = 10 * timeEnd + 1
 times = np.linspace(0, timeEnd, num=n_times)
 
@@ -34,15 +34,15 @@ for iter in iterations:
     model_stoch.set_outputs(seir)
     
     output = model_stoch.simulate(np.array(parameter_values), times)
-    stoch_infections[:, i] = output[:, 2]
+    stoch_infections[:, i] = np.nan_to_num(output[:, 2])
     i+=1
 
  # set up the deterministic model
 
 
-infected_stoch_mean = np.mean(stoch_infections, axis=0)
-infected_stoch_2_5th_quartile = np.percentile(stoch_infections, 2.5, axis=0)
-infected_stoch_97_5th_quartile = np.percentile(stoch_infections, 97.5, axis=0)
+infected_stoch_mean = np.nanmean(stoch_infections, axis=1)
+infected_stoch_2_5th_quartile = np.nanpercentile(stoch_infections, 2.5, axis=1)
+infected_stoch_97_5th_quartile = np.nanpercentile(stoch_infections, 97.5, axis=1)
 model_determ._parameters.configure_parameters(parameter_values)
 model_determ.set_outputs(seir)
 output = model_determ.simulate(np.array(parameter_values), times)
@@ -50,10 +50,12 @@ infection[:,0] = output[:, 2]
 
 figure = se.plots.ConfigurablePlotter()
 figure.begin()
-figure.add_data_to_plot(times, infection, xlabel='time', ylabels='number of infected')
+figure.add_data_to_plot(times, infection, xlabel='time', ylabels=['deterministic'], colours=['r'])
 print(infected_stoch_mean.shape)
-figure.add_data_to_plot(times, infected_stoch_mean, xlabel='time', ylabels='number of infected')
-figure.add_data_to_plot(times, infected_stoch_2_5th_quartile, xlabel='time', ylabels='number of infected')
-figure.add_data_to_plot(times, infected_stoch_97_5th_quartile, xlabel='time', ylabels='number of infected')
+figure.add_data_to_plot(times, infected_stoch_mean, xlabel='time', ylabels=['stochastic'], colours=['b'])
+figure.add_data_to_plot(times, infected_stoch_2_5th_quartile, xlabel='time', ylabels=['2.5th percentile'], colours=['b'])
+figure.add_data_to_plot(times, infected_stoch_97_5th_quartile, xlabel='time', ylabels=['97.5th percentile'], colours=['b'])
+figure.add_fill(times, infected_stoch_2_5th_quartile, infected_stoch_97_5th_quartile,
+                xlabel='times', ylabel='stochastic interval')
 
 figure.show()
