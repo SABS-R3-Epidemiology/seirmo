130fe4f6d8faa5bdaf50d967fd96f6080c1a9181
richard <richard.creswell@hertford.ox.ac.uk>
2021-10-29 11:07:05 +0100

    run stochastic simulations with new parameters

--- multiple_stoch_vs_determ.py
+++ multiple_stoch_vs_determ.py
@@ -7,21 +7,21 @@
 import seirmo as se
 import numpy as np
 import pints as p
+import matplotlib.pyplot as plt
 
 model_stoch = se.StochasticSEIRModel(['S0', 'E0', 'I0', 'R0', 'beta', 'kappa',
                                       'gamma'])
 model_determ = se.DeterministicSEIRModel()
 seir = ['S', 'E', 'I', 'R']
-iteration_number = 101
+iteration_number = 301
 iterations = np.linspace(0, 100, iteration_number)
-parameter_values = np.array([1000, 0, 20, 0, .1, .1, .1])
+parameter_values = np.array([2000, 0, 1, 0, .005, 0.66, 0.33])
 infected_stoch_mean = np.zeros((iteration_number, 1))
 infected_stoch_2_5th_quartile = np.zeros((iteration_number, 1))
 infected_stoch_97_5th_quartile = np.zeros((iteration_number, 1))
-timeEnd = 20
+timeEnd = 10
 n_times = 10 * timeEnd + 1
 times = np.linspace(0, timeEnd, num=n_times)
-
 
 stoch_infections = np.zeros((n_times, iteration_number))
 infection = np.ndarray((n_times,1))
@@ -39,7 +39,6 @@ for iter in iterations:
 
  # set up the deterministic model
 
-
 infected_stoch_mean = np.nanmean(stoch_infections, axis=1)
 infected_stoch_2_5th_quartile = np.nanpercentile(stoch_infections, 2.5, axis=1)
 infected_stoch_97_5th_quartile = np.nanpercentile(stoch_infections, 97.5, axis=1)
@@ -47,6 +46,19 @@ model_determ._parameters.configure_parameters(parameter_values)
 model_determ.set_outputs(seir)
 output = model_determ.simulate(np.array(parameter_values), times)
 infection[:,0] = output[:, 2]
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(times, stoch_infections[:,0], color='blue', alpha=0.08, label='Stochastic')
+ax.plot(times, stoch_infections[:,1:], color='blue', alpha=0.08)
+ax.plot(times, infection, color='red', label='Deterministic', lw=3)
+ax.legend()
+ax.set_xlabel('Time')
+ax.set_ylabel('Infected compartment')
+plt.show()
+exit()
+
 
 figure = se.plots.ConfigurablePlotter()
 figure.begin()
